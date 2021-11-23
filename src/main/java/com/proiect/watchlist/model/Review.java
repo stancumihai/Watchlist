0e3bb5f6da2a7e1d13e4796946992effc9d759f2
Larisa Gabriela Priscuri <priscurilarisagabriela@yahoo.com>
2021-11-23 22:23:12 +0200

    update commit

---
+++ src/main/java/com/proiect/watchlist/model/Review.java
@@ -0,0 +1,46 @@
+package com.proiect.watchlist.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @SequenceGenerator(
+            name = "movie_sequence",
+            sequenceName = "movie_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "movie_sequence"
+    )
+    private Integer id;
+
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JsonIgnore
+    private User user;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "movie_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Movie movie;
+
+}
